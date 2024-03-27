539230ebe3b07c5ca8f59e83bde24a9c1025fece
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-03-27 00:13:52 -0400

    without predefined function to compute tournament winner

---
+++ TournamentWinnerWithoutLinq.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class TournamentWinnerWithoutLinq {
+  public string TournamentWinner(
+    List<List<string> > competitions, List<int> results
+  ) {
+    string winner = "";
+    string homeTeam = "";
+    string awayTeam = "";
+    string currentBest = "";
+    Dictionary<string,int> pointsDict = new Dictionary<string,int>();
+    pointsDict[currentBest] = 0;
+    for(int i=0; i < competitions.Count; i++)
+    {
+        string[] competitionsArray = competitions[i].ToArray();
+        homeTeam = competitionsArray[0]; awayTeam  = competitionsArray[1];
+        winner = results[i] == 0 ? awayTeam : homeTeam;
+        if(pointsDict.TryGetValue(winner, out int points))
+        {
+            pointsDict[winner] = points + 3;
+        }
+        else
+        {
+            pointsDict[winner] = 3;
+        }
+        if(pointsDict[winner] > pointsDict[currentBest])
+            currentBest = winner;
+    }
+    return currentBest;
+  }
+}
