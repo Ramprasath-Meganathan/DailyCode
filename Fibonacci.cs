004889f9ce90edbebd94705a9bbce61461a27ce0
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-04-12 20:19:34 -0400

    added fibonacci program

---
+++ Fibonacci.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class FibonacciPgm {
+  public static int GetNthFib(int n) {
+    if(n==1)
+        return 0;
+    else if(n==2)
+        return 1;
+    else
+        return GetNthFib(n-1)+GetNthFib(n-2);
+        
+    return -1;
+  }
+}
