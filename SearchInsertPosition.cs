38d2beebe9be6d437aea1205ad95dd2fae7a4563
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-07-20 23:47:51 -0400

    search insert position unfinished

---
+++ SearchInsertPosition.cs
@@ -0,0 +1,17 @@
+public class SearchInsertPosition {
+    public int SearchInsert(int[] nums, int target) {
+       int min = 0, max = nums.Length - 1;
+       int mid = 0;
+       while(min<max)
+       {
+        mid = min+max/2;
+       if(target == nums[mid])
+            return mid;
+       else if(target<nums[mid])
+            max = mid-1;
+        else
+            min = mid+1;
+        }
+    return min;
+    }
+}
