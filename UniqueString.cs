2b802b89428735078f81af39bf2897592f9e2916
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-11-29 02:24:40 -0500

    updated alternate solutions for finding if string contains unique char

--- UniqueString.cs
+++ UniqueString.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 // O(n) time and O(1) space
+
+// solution 1
 public static class UniqueString {
     public static bool FindUniqueString(string s) {
         Dictionary<char,int> uniqueCounter = new Dictionary<char,int>();
@@ -13,6 +18,36 @@ public static class UniqueString {
         }
         return true;
     }
+
+    // O(n) time and O(1) space
+    public static bool FindUniqueStringSolution2(string s)
+    {
+        bool[] charSet= new bool[128]; //ASCII assumption
+        for(int i=0; i< s.Length; i++)
+        {
+            int val = s[i];
+            if(charSet[val])
+                return false;
+            else
+                charSet[val] = true;
+        }
+        return true;
+    }
+
+    // O(n) time and O(1) space
+    public static bool FindUniqueStringSolution3(string s)
+    {
+        HashSet<int> charSet= []; 
+        for(int i=0; i< s.Length; i++)
+        {
+            int val = s[i];
+            if(charSet.Contains(val))
+                return false;
+            charSet.Add(val);
+        }
+        return true;
+    }
 }
+
 
 // Find if string has unique characters
