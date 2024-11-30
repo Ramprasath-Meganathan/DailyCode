14e98c117da56f1759653784beac02362d2863c8
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-11-30 02:04:37 -0500

    permutation string optimal solution

--- PermutationString.cs
+++ PermutationString.cs
@@ -2,14 +2,37 @@ using System.Collections.Immutable;
 
 public static class PermutationString
 {
+    // O(nlogn) time
     public static bool PermutationStringSolution(string str1, string str2)
     {
-        var sortedStr1 = str1.ToCharArray();
-        Array.Sort(sortedStr1);
-        var sortedStr2 = str1.ToCharArray();
-        Array.Sort(sortedStr2);
-        var newStr1 = new string(sortedStr1);
-        var newStr2 = new string(sortedStr2);
-        return newStr1.Equals(newStr2);
+        if(str1.Length != str2.Length)
+            return false;
+        return Sort(str1).Equals(Sort(str1));
+    }
+
+    private static string Sort(string str)
+    {
+        var sortedStr = str.ToCharArray();
+        Array.Sort(sortedStr);
+        return new string(sortedStr);
+    }
+
+    // O(n) time 
+    public static bool PermutationStringSolution2(string str1, string str2)
+    {
+        if(str1.Length != str2.Length)
+             return false;
+        
+        int[] letters = new int[128];
+        for(int i = 0; i < str1.Length; i++)
+            letters[str1[i]]++;
+        for(int i = 0; i < str2.Length; i++)
+        {
+            letters[str2[i]] --;
+            if(letters[str2[i]]<0)
+                return false;
+        }
+            return true;
+       
     }
 }
