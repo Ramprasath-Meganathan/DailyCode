df2e0a27b342a5dfc0c4515e801c3660f6ddaf7e
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-12-16 00:09:41 -0500

    print kth to last using two pointers

--- PrintKthToLast.cs
+++ PrintKthToLast.cs
@@ -10,4 +10,22 @@ class PrintKthToLast{
             Console.Write(k+"th element to last node is "+ head.data);
         return index;
     }
+
+    // O(n) time and O(1) space
+    public Node PrintKthToLastTwoPointers(Node head, int k)
+    {
+        Node p1 = head, p2 = head;
+        for(int i = 0;i < k; i++)
+        {
+            if(p1== null)
+                return null;
+            p1 = p1.Next;
+        }
+        while(p1 !=null)
+        {
+            p1 = p1.Next;
+            p2 = p2.Next;
+        }
+        return p2;
+    }
 }
