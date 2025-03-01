b5d411fdb8f133a3073ecebfb870555ac002ffab
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2025-03-01 01:31:20 -0500

    best time to buy a stock two pointer

--- BestTimeToBuyAStock.cs
+++ BestTimeToBuyAStock.cs
@@ -40,6 +40,23 @@ public class BestTimeToBuyAStock {
 
         return maxProfit;
     }
+
+    // O(n) time and O(1) space
+    public int MaxProfitTwoPointer(int[] prices) {
+        int left = 0, right = 1, maxProfit = 0;
+        while(right<prices.Length)
+        {
+            if(prices[left]<prices[right])
+            {
+            int profit = prices[right] - prices[left];
+            maxProfit = Math.Max(maxProfit, profit);
+            }
+            else
+                left = right;
+            right+=1;
+        }
+        return maxProfit;
+}
 }
 
 
