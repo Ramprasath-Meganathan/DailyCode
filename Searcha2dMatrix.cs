c27a1901edb87af0092b3d163b8c764e31a6b502
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2025-03-05 01:34:51 -0500

    searcha2dmatrix

---
+++ Searcha2dMatrix.cs
@@ -0,0 +1,32 @@
+public class Search2DMatrix {
+    //O(m*n) time and O(1) space
+    public bool SearchMatrix(int[][] matrix, int target) {
+       foreach(var row in matrix)
+       {
+        foreach(var num in row)
+        {
+            if(num==target)
+                return true;
+        }
+       }
+       return false;
+    }
+
+    // O(log(m*n)) time and O(1) space
+     public bool SearchMatrixBinarySearc(int[][] matrix, int target) {
+       int row = matrix.Length, col = matrix[0].Length;
+       int left = 0, right = row*col-1;
+       while(left<=right)
+       {
+        int mid = left+(right-left)/2;
+        int midValue = matrix[mid/col][mid%col];
+        if(midValue==target)
+            return true;
+        if(midValue<target)
+            left = mid+1;
+        else
+            right = mid-1;
+       }
+       return false;
+    }
+}
