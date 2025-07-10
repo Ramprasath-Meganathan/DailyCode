ea417b44d08e87887df6447b293d85100c8acab1
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2025-07-09 21:34:50 -0400

    subsets iterative solution

--- Marathon/Subsets.py
+++ Marathon/Subsets.py
@@ -16,6 +16,13 @@ class solution:
             backtrack(0)
             return res
 
+#iterative solution
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in nums:
+                res+=[subset+[num] for subset in res]
+        return res
+
 #Given an integer array nums of unique elements, return all possible subsets (the power set).
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
